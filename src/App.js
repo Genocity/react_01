76d2feb6c197486c7135496b4c07c08aeca50c66
Genocity <gumption1543@gmail.com>
2020-02-13 19:02:18 +0200

    added list of books

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Books from "./components/Books";
 
 function App() {
   return (
     <div className="App">
-      some text
+      <Books />
     </div>
   );
 }
---
+++ src/components/Books.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Books = () => {
+    return (
+        <ul>
+            <li>Antifragile</li>
+            <li>Pale fire</li>
+            <li>Old man and sea</li>
+            <li>1984</li>
+            <li>The Prince</li>
+        </ul>
+    );
+};
+
+export default Books;
